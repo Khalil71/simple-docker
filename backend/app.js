8a42edbacfe22d48bfc43da4681bbb495a5434f7
Khalil71 <mohamed.khalil609@gmail.com>
2018-04-14 01:12:45 +0200

    Quick fix

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
-var express = require("express");
+var express = require('express');
 
 // Constants
 var PORT = 8080;
 
 // App
 var app = express();
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.send({
-    data: { message: "This is the message", status: 200 },
+    data: { message: 'This is the message', status: 200 },
     data2: "This shouldn't be sent to the frontend"
   });
 });
 
 app.listen(PORT);
-console.log("Running on port: " + PORT);
+console.log('Running on port: ' + PORT);
--- frontend/app.js
+++ frontend/app.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
-var express = require("express");
-var axios = require("axios");
+var express = require('express');
+var axios = require('axios');
 
 // Constants
 var PORT = 9090;
 
 // App
 var app = express();
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   return axios
-    .get("http://lb-backend")
+    .get('http://lb-backend')
     .then(function(data) {
       return res.send(data.data.data);
     })
@@ -20,4 +20,4 @@ app.get("/", function(req, res) {
 });
 
 app.listen(PORT);
-console.log("Running on port: " + PORT);
+console.log('Running on port: ' + PORT);
